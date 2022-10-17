4f5147d90a88c31d433353cbd764363706d5d3d1
fleshel <jfleshel@temple.edu>
2022-10-17 11:16:01 -0400

    finished simple loading with console logging

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# kucoin-balance-tracker
-a tool to track your daily balance on the KuCoin platform. 
+# KuCoin Balance Tracker
+A tool to track your daily balance on the KuCoin platform. 
+
+## installation & usage
+
+### environment variables
+you need to update your environment variables prior to using. this can be done by modifying the .env file
+
+- KUCOIN\_API_KEY=***
+- KUCOIN\_API_SECRET=***
+- KUCOIN\_API_PASSPHRASE=***
+
+### external libraries
+there are a few packages necessary to install prior to running.
+
+- python_dotenv
+    - used to load your api key from .env
+
+- requests
+    - used to request balance from the KuCoin API
+
+you can install these with
+`pip install python_dotenv requests` if they have not been installed already.
+
+### usage
+in order to run the application, simply enter `python3 main.py`
---
+++ main.py
@@ -0,0 +1,113 @@
+import os
+import time
+import base64
+import hmac
+import hashlib
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import requests
+
+#   import boto3
+#   from botocore.config import Config
+
+BASE_URL = "https://api.kucoin.com"
+
+API_KEY = os.getenv('KUCOIN_API_KEY')
+api_secret = os.getenv('KUCOIN_API_SECRET')
+api_passphrase = os.getenv('KUCOIN_API_PASSPHRASE')
+
+endpoints = {
+    "GET_ACCOUNT": "/api/v1/accounts",
+    "TIMESTAMP": "/api/v1/timestamp"
+}
+
+#Returns -- AWS Connection
+'''
+def connect_to_aws():
+    aws_config = Config(
+        region_name = 'us-east-1',
+        signature_version = 'v4',
+    )
+
+    dynamo = boto3.client('dynamodb',
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+        config=aws_config)
+
+    return dynamo
+'''
+
+def load_data():
+    now = int(time.time() * 1000)
+    str_to_sign = str(now) + 'GET' + '/api/v1/accounts'
+
+    signature = base64.b64encode(
+        hmac.new(api_secret.encode('utf-8'), str_to_sign.encode('utf-8'), 'sha256').digest())
+
+    passphrase = base64.b64encode(hmac.new(api_secret.encode('utf-8'), api_passphrase.encode('utf-8'), 'sha256').digest())
+
+    headers = {
+        "KC-API-SIGN": signature,
+        "KC-API-TIMESTAMP": str(now),
+        "KC-API-KEY": API_KEY,
+        "KC-API-PASSPHRASE": passphrase,
+        "KC-API-KEY-VERSION": "2"
+    }
+
+    response = requests.request('get', BASE_URL+endpoints['GET_ACCOUNT'], headers=headers)
+
+    print('request completed with status code={sc}'.format(sc=response.status_code))
+
+    response_data = response.json()['data'] if response.json()['code'] == '200000' else []
+
+    print('fetched data for {n} accounts'.format(n=len(response_data)), end='\n\n')
+
+    balances = {}
+    skipped_accts = 0
+
+    for a in range(len(response_data)):
+        #   print('account: {act}'.format(act=account))
+        account = response_data[a]
+        
+        curr = account['currency']
+        balance = account['balance']
+
+        if curr in balances.keys():
+            balances[curr] += balance
+        else:
+            balances[curr] = balance
+
+
+        if float(balance) > 0:
+            print('[account {n} |{type}] bal: {bal} / {avl} avail ({cxy})'.format(
+                n=a+1,
+                type=account['type'].upper(),
+                bal=account['balance'],
+                avl=account['available'],
+                cxy=account['currency']),
+                end='\n\n'
+            )
+        else:
+            skipped_accts += 1
+
+    if skipped_accts > 0:
+        print('\nSkipped {n} emtpy accounts.'.format(n=skipped_accts),end='\n\n')
+    print('BALANCE SUMMARY:')
+
+    hidden_bals = 0
+    for k in balances.keys():
+        balance = balances[k]
+        if float(balance) > 0:
+            print('{cxy}: {bal}'.format(cxy=k, bal=balances[k]))
+        else:
+            hidden_bals += 1
+        
+    print('\nSkipped {n} empty coins'.format(n=hidden_bals))
+
+    #dynamo = connect_to_aws()
+
+    #print(dynamo)
+
+load_data()
